06e26818ef6a103299444a915b316a3220396ae2
ysa23 <ysa.angel@gmail.com>
2018-05-13 11:49:58 +0300

    Add solution

---
+++ src/YsA.NuGet.FrameworkSupportTester/Options.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+
+namespace NuGetReader
+{
+	public class Options
+	{
+		[Option('n', "name", Required = true, Default = "", HelpText = "Name of the NuGet package")]
+		public string Name { get; set; }
+		[Option('n', "version", Required = true, Default = "", HelpText = "Version of the NuGet package")]
+		public string Version { get; set; }
+		[Option('f', "framework", Required = false, Default = ".NETStandard", HelpText = "Framework to test")]
+		public string Framework { get; set; }
+	}
+}
---
+++ src/YsA.NuGet.FrameworkSupportTester/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CommandLine;
+using NuGet.Packaging;
+
+namespace NuGetReader
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Parser.Default.ParseArguments<Options>(args)
+				.WithParsed(Run);
+		}
+
+		private static void Run(Options opt)
+		{
+			var userProfile = GetUserProfileDirectory();
+
+			var packages = new List<(string Name, string Version)>
+			{
+				(opt.Name, opt.Version)
+			};
+
+			foreach (var packageDetails in packages)
+			{
+				var packagePath = Path.Combine(userProfile, ".nuget", "packages", packageDetails.Name, packageDetails.Version);
+
+				if (!Directory.Exists(packagePath))
+				{
+					Console.WriteLine($"{packageDetails.Name}\t{packageDetails.Version}\tNot found locally. Please download it first.");
+					continue;
+				}
+				
+				var nugetFilePath = Directory.GetFiles(packagePath, "*.nupkg", SearchOption.AllDirectories).FirstOrDefault();
+				if (nugetFilePath == null)
+				{
+					Console.WriteLine($"{packageDetails.Name}\t{packageDetails.Version}\tNot found locally. Please download it first.");
+					continue;
+				}
+
+				CheckPackage(nugetFilePath, opt.Framework, packageDetails);
+			}
+		}
+
+		private static string GetUserProfileDirectory()
+		{
+			var userProfile = Environment.GetEnvironmentVariable("userprofile");
+			if (string.IsNullOrEmpty(userProfile))
+				userProfile = "~";
+			return userProfile;
+		}
+
+		private static void CheckPackage(string nugetFilePath, string framework, (string Name, string Version) packageDetails)
+		{
+			using (var package = new PackageArchiveReader(nugetFilePath))
+			{
+				var frameworks = package
+					.GetSupportedFrameworks()
+					.Where(f => f.DotNetFrameworkName.IndexOf(framework, StringComparison.OrdinalIgnoreCase) >= 0)
+					.ToArray();
+
+				Console.WriteLine(frameworks.Length == 0
+					? $"{packageDetails.Name}\t{packageDetails.Version}\tNo support for .Net standard"
+					: $"{packageDetails.Name}\t{packageDetails.Version}\t{string.Join(";", (object[]) frameworks)}");
+			}
+		}
+	}
+}
